88c223036b98a059cd8a0b0221d7b11c3d2b12c1
Simone Westphal <s.westphal@dennree.de>
2022-11-24 18:42:46 +0100

    refactor: added post and comment components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 
 const App = () => {
@@ -7,6 +8,9 @@ const App = () => {
     <div className='container'>
         <h1>Create Post</h1>
         <PostCreate/>
+        <hr/>
+        <h1>Posts</h1>
+        <PostList/>
     </div>)
 }
 
