7300d02468c7e59957911100bf3004e9fe28c00e
Simone Westphal <s.westphal@dennree.de>
2022-11-24 18:41:14 +0100

    refactor: added comments functionality

---
+++ client/src/PostList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const PostList = () => {
+    const [posts, setPosts] = useState({});
+
+    const fetchPosts = async () => {
+        const res = await axios.get('http://localhost:4000/posts')
+
+        setPosts(res.data)
+    }
+
+    useEffect(()=> {
+            fetchPosts()
+    },[])
+
+    const renderedPosts = Object.values(posts);
+
+    return (
+        <div className='d-flex flex-row flex-wrap justify-content-between'>
+            {renderedPosts.map((post, index) => {
+                return(
+                    <div key={post.id} className='card' style={{width:'30%', marginBottom:'20px'}}>
+                        <div className='card-body'>
+                            <p>{post.title}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default PostList;
